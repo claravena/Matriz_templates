93f29ec6ea16788936e71098378b2e9c1304c2d9
claudio <claudio@Dirac>
2018-08-12 22:19:00 -0300

    se agregaron operaciones elementales de filas, como cambio de filas, suma ponderada de filas y multiplicación de filas

--- Matriz.h
+++ Matriz.h
@@ -15,17 +15,24 @@ class Matriz{
   Matriz(int, int);
   Matriz(int, int,vector<T>);
   ~Matriz();
-  void set_matriz(vector<T>);
-  int get_f() ; 
-  int get_c() ;
-  int get_dim() ;
+  void set_matriz(vector<T>); //carga los elementos de un vector en un matriz.
+  int get_f() ; //entrega el numero de filas.
+  int get_c() ; //entrega el numero de columnas. 
+  int get_dim(); //entrega la dimension fxc.
   T traza(); 
-  vector<T> get_mt() const ;
-  Matriz(const Matriz &);
-  T & operator()(int, int);
-  T operator()(int, int) const;
-  Matriz & operator = (const Matriz<T> &); 
+  vector<T> get_fila(int); // entrega en un vector la fila seleccionada. 
+  vector<T> get_col(int); // entrega en un vector la columna seleccionada.
+  vector<T> get_mt() const ; //entrega en un vector todos los elementos de la matriz. 
+  Matriz(const Matriz &); //contructor de copia. 
+  T & operator()(int, int); //operador que entrega el elemento f,c seleccionado.
+  T operator()(int, int) const; //operador que entrega el elemento f,c seleccionado.
+  Matriz & operator = (const Matriz<T> &); //operador asignación. 
+  Matriz<T> inter_fila(int, int); //intercambia las filas.
+  Matriz<T> multiplicar_fila(T, int); // multiplica por un numero de tipo T la fila int selecionada.
+  Matriz<T> suma_fila(int, int, T x); //le suma a la fila f2, el resultado de x*f1.
+  Matriz<T> ordenar(); //
 };
+
 
 #endif
 
@@ -41,7 +48,6 @@ Matriz<T>::Matriz(int a,int b){
     }
 }
   
-
 //Constructor 
 template <class T>
 Matriz<T>::Matriz(int a,int b,vector<T> A){
@@ -57,9 +63,7 @@ Matriz<T>::Matriz(int a,int b,vector<T> A){
 	mt.push_back(0);
       }
   }
-  
 }
-
 
 //Destructor
 template <class T>
@@ -111,7 +115,7 @@ void Matriz<T>::set_matriz(vector<T> A){
   mt=A; 
 }
 
-//Retorna el elemento A(i,j)
+//Retorna el elemento A(i,j) i=0,1,...f-1 y j=0,1, ... c-1. 
 template <class T>
 T & Matriz<T>::operator()(int i, int j)
 {
@@ -133,6 +137,74 @@ Matriz<T>::Matriz(const Matriz<T> & A){
   vector<T> v_copia = A.get_mt();
   mt= v_copia;
 }
+
+//Retorna la fila de una matriz. 
+template <class T>
+vector<T> Matriz<T>::get_fila(int numero){
+  Matriz<T> M(f,c,mt);
+  vector<T> vec_fila;
+  for(int j=0; j<c; ++j){
+    vec_fila.push_back(M(numero,j)); ; 
+  }
+  return vec_fila; 
+}
+
+//Retorna la columna de una matriz. 
+template <class T>
+vector<T> Matriz<T>::get_col(int numero){
+  Matriz<T> M(f,c,mt); 
+  vector<T> vec_col;
+  for(int i=0; i<f; ++i){
+    vec_col.push_back(M(i,numero)); 
+  }
+  return vec_col; 
+}
+//intercambia hacia arriba la fila f2 con la f1. 
+template <class T>
+Matriz<T> Matriz<T>::inter_fila(int f1, int f2){
+  vector<T> copia_mt=mt; 
+  Matriz<T> M(f,c,copia_mt);
+  for(int j=0; j<c ; ++j ){
+    mt[f1*c+j]=T(-1)*M(f2,j);
+    mt[f2*c+j]=T(-1)*M(f1,j); 
+  };
+  return *this;  
+}
+//multiplica por un numero de tipo T una fila f seleccionada. 
+template <class T>
+Matriz<T> Matriz<T>::multiplicar_fila(T x, int f1){
+  vector<T> copia_mt=mt; 
+  Matriz<T> M(f,c,copia_mt);
+  for(int j=0; j<c ; ++j){
+    mt[f1*c+j]=x*M(f1,j);
+  };
+  return *this; 
+}
+
+template <class T>
+Matriz<T> Matriz<T>::suma_fila(int f2, int f1, T x){
+  vector<T> copia_mt=mt; 
+  Matriz<T> M(f,c,copia_mt);
+  for(int j=0; j<c ; ++j){
+    mt[f2*c+j]= M(f2,j)+x*M(f1,j); 
+  };
+  return *this; 
+}
+
+template <class T>
+Matriz<T> Matriz<T>::ordenar(){
+  vector<T> copia_mt=mt;
+  Matriz<T> M(f,c,copia_mt);
+  for(int i=0; i<c; ++i){
+    if(M(i,0)==0){
+      M.inter_fila(i,(i+1));
+    }
+  }
+  mt=M.get_mt();
+  return *this; 
+}
+
+
 
 
 
@@ -166,10 +238,6 @@ Matriz<T> &Matriz<T>::operator = (const Matriz<T> & M){
 }
 
 
-
-
-
-
 //-------------------------------Operador Suma----------------------------------
 template <class T>
 Matriz<T> operator + (Matriz<T> X, Matriz<T> Y){
@@ -190,27 +258,8 @@ Matriz<T> operator + (Matriz<T> X, Matriz<T> Y){
   }
 }
 
-//--------------------------------Operador Resta--------------------------------
-template <class T>
-Matriz<T> operator - (Matriz<T> X, Matriz<T> Y){
-  if (X.get_f() == Y.get_f() && X.get_c() == Y.get_c()){
-    vector<T> v_sum;
-    int fila=X.get_f();
-    int col=X.get_c();
-    for (int i=0;i<fila;++i){
-      for (int j=0;j<col;++j){
-	v_sum.push_back(X(i,j)-Y(i,j)); 
-      } 
-    }
-    Matriz<T> C(fila,col,v_sum);
-    return C;
-  }
-  else{
-    cout<<" No se puede realizar la suma"<<endl;
-  }
-}
 
-//---------------------------Operador multiplicacion por escalar---------------
+//---------------------------Operador multiplicacion por escalar a*T---------------
 template <class T>
 Matriz<T> operator * (T x, Matriz<T> Y){
   T escalar = x;
@@ -226,6 +275,22 @@ Matriz<T> operator * (T x, Matriz<T> Y){
   return C;
 }
 
+//---------------------------Operador multiplicacion por escalar T*a---------------
+template <class T>
+Matriz<T> operator * (Matriz<T> Y, T x){
+  return x*Y;
+}
+
+//--------------------------------Operador Resta--------------------------------
+template <class T>
+Matriz<T> operator - (Matriz<T> X, Matriz<T> Y){
+  if (X.get_f() == Y.get_f() && X.get_c() == Y.get_c()){
+    return X+(T(-1)*Y);
+  }
+  else{
+    cout<<" No se puede realizar la suma"<<endl;
+  }
+}
 
 
 
@@ -256,4 +321,6 @@ Matriz<T> operator * (Matriz<T> X, Matriz<T> Y){
       return  ;*/ 
   }
 }
+
+
 
